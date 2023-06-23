a49dc9ce2acf5260c0ce36ba17c9e9a221766269
Fabiano Vasconcelos <fvasconcelos@gmail.com>
2023-06-23 17:06:14 -0300

    Tentativa de fazer a API funcionar

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const api_url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
 
 function App() {
   const titleStyle = {
     fontFamily: 'Arial, sans-serif',
   };
 
+  const [jsonData, setJsonData] = useState(null);
+
+  useEffect(() => {
+    logJSONData();
+  }, []);
+
+  const logJSONData = async () => {
+    const response = await fetch(api_url);
+    const jsonData = await response.json();
+    setJsonData(jsonData);
+  }
+
   return (
     <>
       <div className='background-image'>
         <h1 style={titleStyle}>🐭 White Mouse ☀️ Clima & Tempo ⛅</h1>
-      </div>
+        {jsonData && (
+          <div>
+            <p>Temperatura: {jsonData.temperature}</p>
+            <p>Umidade: {jsonData.humidity}</p>
+          </div>
+        )}
+        </div>
     </>
   )
 }
