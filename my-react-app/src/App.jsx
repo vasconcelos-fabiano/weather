0df3316a4ab89d07fc701cec26960d82ba1bc770
Fabiano Vasconcelos <63758332+vasconcelos-fabiano@users.noreply.github.com>
2023-06-28 23:23:25 -0300

    Finalmente funcionando

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-const api_url = "https://api.openweathermap.org/data/3.0/onecall?lat=-3.73&lon=-38.54&exclude=current,minutely,daily,alerts&appid=93ecdd97bfb1bc86ab41c2860a5649a8";
+// const api_url = "https://api.openweathermap.org/data/3.0/onecall?lat=-3.73&lon=-38.54&exclude=current,minutely,daily,alerts&appid=93ecdd97bfb1bc86ab41c2860a5649a8";
+const api_url = "https://api.openweathermap.org/data/2.5/weather?lat=-3.73&lon=-38.54&units=metric&lang=en&appid=93ecdd97bfb1bc86ab41c2860a5649a8"
 
 function App() {
   const titleStyle = {
@@ -18,7 +19,7 @@ function App() {
     const response = await fetch(api_url);
     const jsonData = await response.json();
     setJsonData(jsonData);
-    console.log(jsonData.current);
+    console.log(jsonData.main.temp);
   }
 
   return (
@@ -28,14 +29,14 @@ function App() {
         {jsonData && (
           <div>
             <p>Cidade<br/><input type="text" id='city' placeholder='Digite aqui a sua cidade'></input></p><br></br>
-            <p>Temperatura:  C˚</p>
-            <p>Vento:  Km/h</p>
-            <p>Sensação Térmica:  C˚</p>
-            <p>Umidade do ar:  %</p>
-            <p>Temperatura máxima:  </p>
-            <p>Temperatura mínima:  </p>
-            <p>Pôr-do-sol:  </p>
-            <p>Amanhecer:  </p>
+            <p>Temperatura: {jsonData.main.temp} C˚</p>
+            <p>Vento: {jsonData.wind.speed} Km/h</p>
+            <p>Sensação Térmica: {jsonData.main.feels_like} C˚</p>
+            <p>Umidade do ar: {jsonData.main.humidity} %</p>
+            <p>Temperatura máxima: {jsonData.main.temp_max} C˚</p>
+            <p>Temperatura mínima: {jsonData.main.temp_min} C˚</p>
+            <p>Pôr-do-sol: {jsonData.sys.sunset} </p>
+            <p>Amanhecer: {jsonData.sys.sunrise} </p>
           </div>
         )}
         </div>
